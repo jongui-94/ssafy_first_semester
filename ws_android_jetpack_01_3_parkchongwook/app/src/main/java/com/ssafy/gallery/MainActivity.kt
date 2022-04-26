3d08d30d5fe712b24da69af848abf1e214dc2759
sa01070 <sa01070@naver.com>
2022-04-27 02:14:40 +0900

    0426homework

--- ws_android_jetpack_01_3_parkchongwook/app/src/main/java/com/ssafy/gallery/MainActivity.kt
+++ ws_android_jetpack_01_3_parkchongwook/app/src/main/java/com/ssafy/gallery/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ssafy.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ssafy.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, GalleryFragment.newInstance(this)).commit()
+
+
     }
+
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment).commit()
+    }
+
 }
